906d7ae62938efcaabcb2211728eab794564c5c7
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-16 02:29:30 +0600

    signup account creation and redirect to the home page and login signin with existing account and redirect to the orders page done. These done with ready made react custom authentication hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,54 @@
-import { FontAwesomeIcon,} from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+        console.log(email)
+    };
+    const handPAsswordBlur = event => {
+        setPassword(event.target.value);
+        console.log(password)
+
+    };
+
+    const handleLogin = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    };
+    if (user) {
+        navigate('/orders');
+    }
+
+
     return (
         <div className='form-container'>
-
-            <form >
-                <h2 className='form-title'>Login</h2>
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" />
-                    <label htmlFor="password">Password</label>
-                    <input type="password" />
-                </div>
-                <input className='form-submit' type="submit" value={'Login'} />
+            <div>
+                <form onSubmit={handleLogin} >
+                    <h2 className='form-title'>Login</h2>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handPAsswordBlur} type="password" name="password" id="" />
+                        <p style={{ color: 'red' }}>{error?.message}</p>
+                    </div>
+                    <input className='form-submit' type="submit" value={'Login'} />
+                </form>
                 <p>
                     New to Ema-john? <Link className='link-navigation' to='/signup'>Create New Account</Link>
                 </p>
@@ -25,7 +58,8 @@ const Login = () => {
                     <div className="line"><hr /></div>
                 </div>
                 <button className='google-login'> <img className='google-logo' src="https://freesvg.org/img/1534129544.png" alt="" /> Continue with Google </button>
-            </form>
+            </div>
+
 
         </div>
     );
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+
+
+    const handleEmailOnBlur = event => {
+        setEmail(event.target.value);
+        console.log(email)
+    };
+    const handlePassOnBlur = event => {
+        setPassword(event.target.value);
+        console.log(email)
+    };
+    const handleConfirmPassOnBlur = event => {
+        setConfirmPassword(event.target.value);
+        console.log(email)
+    };
+
+    if (user) {
+        navigate('/')
+    };
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setErrorMessage('password did not match')
+            return;
+        }
+
+        if (password.length < 6) {
+            setErrorMessage('password must be at least 6 characters')
+        }
+        createUserWithEmailAndPassword(email, password);
+    };
+
+
+
+
     return (
-        <div>
-            <h2>This is signup</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Signup</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailOnBlur} type="email" name="email" id="" required />
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePassOnBlur} type="password" name="password" id="" required />
+                        <label htmlFor="password">Confirm Password</label>
+                        <input onBlur={handleConfirmPassOnBlur} type="password" name="confirmPassword" id="" required />
+                        <p style={{ color: 'red' }}>{errorMessage}</p>
+                    </div>
+                    <input className='form-submit' type="submit" value={'Login'} />
+                </form>
+                <p>
+                    Already have an account ? <Link className='link-navigation' to='/login'>Login</Link>
+                </p>
+                <div className="or-design">
+                    <div className="line"><hr /></div>
+                    <div className="or">or</div>
+                    <div className="line"><hr /></div>
+                </div>
+                <button className='google-login'> <img className='google-logo' src="https://freesvg.org/img/1534129544.png" alt="" /> Continue with Google </button>
+            </div>
+
         </div>
     );
 };
